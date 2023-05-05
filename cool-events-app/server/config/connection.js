1b222a1fbfd0ba7ebc334c235a43a084d8ae11cf
tmcash <t.cash15@yahoo.com>
2023-05-04 20:06:22 -0400

    Added signup and login

--- cool-events-app/server/config/connection.js
+++ cool-events-app/server/config/connection.js
@@ -1,12 +1,12 @@
 
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping', {
+//useNewUrlParser: true,
+//useUnifiedTopology: true,
+//});
 
-module.exports = mongoose.connection;
+//module.exports = mongoose.connection;
 
 
 
