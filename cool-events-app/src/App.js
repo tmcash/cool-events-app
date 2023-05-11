c6fa58533ca45c490b1cb272bb3bceeed58fa0a1
tmcash <t.cash15@yahoo.com>
2023-05-10 21:10:14 -0400

    Added footer and header files for front end

--- cool-events-app/src/App.js
+++ cool-events-app/src/App.js
@@ -12,6 +12,8 @@ import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 // Construct our main GraphQL API endpoint
