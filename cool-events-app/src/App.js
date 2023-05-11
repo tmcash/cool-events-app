73b5cbbc9ffb482c48c45959e2849f998b922059
sswiger <sswiger@forhims.com>
2023-05-11 17:33:25 -0600

    Added calendar functionality

--- cool-events-app/src/App.js
+++ cool-events-app/src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import "./App.css";
+
 import logo from './logo.svg';
 import './App.css';
 
---
+++ cool-events-app/src/pages/Calendar-page.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./App.css";
+
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+import events from "./events";
+
+export default function App() {
+    return (
+        <div className="App">
+        <FullCalendar
+          defaultView="dayGridMonth"
+          header={{
+            left: "prev,next",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay"
+          }}
+          themeSystem="Simplex"
+          plugins={[dayGridPlugin]}
+          events={events}
+        />
+        <FullCalendar
+          defaultView="dayGridMonth"
+        
+          plugins={[dayGridPlugin]}
+          events={events}
+          displayEventEnd="true"
+          eventColor={"#" + Math.floor(Math.random() * 16777215).toString(16)}
+        />
+      </div>     
+    );
+}
