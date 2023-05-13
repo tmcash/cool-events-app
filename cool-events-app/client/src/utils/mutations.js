8d193050c62b74685225074f4a7dc59e0232a947
tmcash <t.cash15@yahoo.com>
2023-05-13 10:25:49 -0400

    Fixed mutation.js

--- cool-events-app/client/src/utils/mutations.js
+++ cool-events-app/client/src/utils/mutations.js
@@ -1,30 +1,20 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-mutation login($email: String!, $password: String!) {
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-    token
-    user {
+      token
+      user {
         _id
         username
+      }
     }
-    }
-}
+  }
 `;
 
 export const ADD_USER = gql`
-  mutation addUser(
-    $firstName: String!
-    $lastName: String!
-    $email: String!
-    $password: String!
-  ) {
-    addUser(
-      firstName: $firstName
-      lastName: $lastName
-      email: $email
-      password: $password
-    ) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
